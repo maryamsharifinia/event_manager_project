403645fb84435e061d9fbf7d8be8350936e01534
maryam <mrymsharifinia@gmail.com>
2023-11-24 11:23:14 +0330

    change_password

--- members/zero/business_flow/user/users_bf.py
+++ members/zero/business_flow/user/users_bf.py
@@ -2,12 +2,15 @@ from helpers.business_flow_helpers import BusinessFlow
 from helpers.config_helper import ConfigHelper
 
 import members as service
+from helpers.io_helpers import *
+from members.zero.utils.utils import *
 
 
 class UserBusinessFlowManager(BusinessFlow):
     def __init__(self, ):
         super(UserBusinessFlowManager, self).__init__(service.service_name)
         self.cfg_helper = ConfigHelper()
+        self.index = self.create_index(self.cfg_helper.get_config(service.service_name)["index_name"])
 
     def select_business_flow(self, data, request, member, params=None):
         self.get_mongo_connection()
@@ -36,10 +39,17 @@ class UserBusinessFlowManager(BusinessFlow):
 
     def update_business_flow(self, data, request, member, params=None):
         self.get_mongo_connection()
-
+        result = {}
+        data = data['data']
         if request["method"] == "update":
-            pass
+            check_schema(data, service.clubmembers_schema)
+            data = preprocess(data, schema=service.clubmembers_schema)
+
+            result = update_member(data, member)
+        elif request["method"] == "change_password":
+            check_required_key(["member_id", "old_password", "new_password"], data)
+            result = change_password(data, member, self.index)
         else:
             raise PermissionError()
 
-        return []
+        return result
