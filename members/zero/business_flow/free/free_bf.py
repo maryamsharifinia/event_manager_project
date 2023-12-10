edf825434211c501fb2f53322097d98aef2fcb76
Mohammadreza <mohammad.mrho@gmai;.com>
2023-12-10 08:57:53 -0800

    forget password

--- members/zero/business_flow/free/free_bf.py
+++ members/zero/business_flow/free/free_bf.py
@@ -64,4 +64,44 @@ class FreeBusinessFlowManager(BusinessFlow):
         raise PermissionError()
 
     def update_business_flow(self, data, request, member, params=None):
-        raise PermissionError()
+        self.get_mongo_connection()
+
+        result = {}
+        data = data['data']
+        if request["method"] == "update":
+            check_schema(data, service.clubmembers_schema)
+            data = preprocess(data, schema=service.clubmembers_schema)
+
+            result = update_member(data, member)
+        elif request["method"] == "change_password":
+            check_required_key(["member_id", "old_password", "new_password"], data)
+            result = change_password(data, member, self.index)
+
+        elif request["method"] == "verify_email":
+            otp_catch = self.db.cache("otp")
+            otp = random.randint(1000, 9999)
+            _id = str(uuid.uuid4())
+            otp_catch.set(key=data["email"], timeout=20 * 60,
+                          value=json.dumps({"correct": otp}))
+            data["content"] = f'کد اعتبارسنجی شما :{otp}'
+            send_email(data, "اعتبارسنجی ایمیل")
+        elif request["method"] == "check_otp_email":
+            if "otp" not in data.keys():
+                raise RequiredFieldError("otp")
+            elif "email" not in data.keys():
+                raise RequiredFieldError("email")
+
+            result = check_otp("email", data, self.db)
+            if result["check"]:
+                doc = {"_id": member["_id"],
+                       "verify_email": "TRUE",
+                       "email": data["email"]
+                       }
+                result = update_member(mongo=self.index, data=doc)
+            else:
+                raise InvalidOtp()
+
+        else:
+            raise PermissionError()
+
+        return result
